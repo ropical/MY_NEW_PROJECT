f71d31717167ab8b4b19fe76aef960f4c47828d4
Gh4t <1714450233@qq.com>
2022-07-15 19:11:05 -0700

    Init Project

---
+++ README.md
@@ -0,0 +1,15 @@
+# 任务
+
+**`void init(struct Player *player)`**
+
+初始化，只执行一次
+
+**`struct Point walk(struct Player *player)`**
+
+重点函数，返回值为蛇头下一步走的位置。
+
+需要通过各种判断确认下一步走的位置，吃更多的道具，获得尽可能高的分数
+
+# 附加题|选做
+
+杀死对方的蛇，获取对方的分数
---
+++ code/player.h
@@ -0,0 +1,32 @@
+/**
+ * @file player.h
+ * @author yangboyang@jisuanke.com
+ * @copyright jisuanke.com
+ * @date 2021-07-01
+ */
+
+#include <string.h>
+#include "../include/playerbase.h"
+#include <stdio.h>
+
+void init(struct Player *player) {
+	// This function will be executed at the begin of each game, only once.
+}
+
+int step[4][2] = {0, 1, 0, -1, 1, 0, -1, 0};
+struct Point walk(struct Player *player) {
+	// for (int i = 0; i < player->row_cnt; i++) {
+	// 	for (int j = 0; j < player->col_cnt; j++) {
+	// 		printf("%c", player->mat[i][j]);
+	// 	}
+	// 	printf("\n");
+	// }
+	// This function will be executed in each round.
+	for (int i = 0; i < 4; i++) {
+		int dx = player->your_posx + step[i][0], dy = player->your_posy + step[i][1];
+		if (dx >= 0 && dx < player->row_cnt && dy >= 0 && dy < player->col_cnt && (player->mat[dx][dy] == '.' || player->mat[dx][dy] == 'o' || player->mat[dx][dy] == 'O')) {
+			return initPoint(dx, dy);
+		}
+	}
+	return initPoint(player->your_posx, player->your_posy);
+}
